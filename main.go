b6dc44186db0a5abe0b6ce9b5d5ad43c9dcf2cb2
Armand Caesar <armand.caesar@gmail.com>
2019-02-02 10:12:23 +0700

    fix rest config

--- main.go
+++ main.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	model "github.com/go-skeleton/Domain/authentication/model"
-	"github.com/golang/protobuf/proto"
+	"github.com/gin-gonic/gin"
+	"github.com/go-skeleton/config"
+	"github.com/go-skeleton/rest"
 )
 
+var (
+	configuration config.Configuration
+	engine        *gin.Engine
+	router        *rest.Router
+)
+
+func init() {
+	cfg, err := config.New("")
+}
+
 func main() {
-
-	elliot := &model.Person{
-		Name: "Elliot",
-		Age:  24,
-	}
-
-	data, err := proto.Marshal(elliot)
-	if err != nil {
-		log.Fatal("marshaling error: ", err)
-	}
-
-	// printing out our raw protobuf object
-	fmt.Println(data)
-
-	// let's go the other way and unmarshal
-	// our byte array into an object we can modify
-	// and use
-	newElliot := &model.Person{}
-	err = proto.Unmarshal(data, newElliot)
-	if err != nil {
-		log.Fatal("unmarshaling error: ", err)
-	}
-
-	// print out our `newElliot` object
-	// for good measure
-	fmt.Println(newElliot.GetAge())
-	fmt.Println(newElliot.GetName())
 
 }
